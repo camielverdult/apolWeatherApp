e515705b1b3d03afbefc993f631f3d9c36ec0247
camielverdult <camiel@camiel.pw>
2021-12-10 14:49:04 +0100

    Initial Commit

---
+++ Shared/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  Shared
+//
+//  Created by Camiel Verdult on 10/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
